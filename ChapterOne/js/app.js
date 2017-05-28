adfdf9d4c51aa80657ea9ea42aff5864dd39085a
MicroKibaco <yangzy3@asiainfo.com>
2017-05-28 19:08:46 +0800

    ItemEditor Removes the function(II)

--- ChapterOne/js/app.js
+++ ChapterOne/js/app.js
@@ -13,10 +13,26 @@ const List = React.createClass({
 
                                    },
 
-                                   add(){
+                                   _add(){
                                        const key = ++this.state.key;
                                        this.state.editList.set(key, {value: ''});
                                        this.forceUpdate();
+                                   },
+
+                                   // remove editList
+
+                                   _removeItemEditor(id){
+                                       this.state.editList.delete(id);
+                                       this.forceUpdate();
+                                   },
+
+                                   // remove list
+
+                                   _removeItem(id){
+
+                                       this.state.list.delete(id);
+                                       this.forceUpdate();
+
                                    },
 
                                    // refresh view
@@ -26,23 +42,24 @@ const List = React.createClass({
 
                                        for (let entry of this.state.list) {
 
-                                           listDOM.push(<Item id={entry[0]} value={entry[1].value}/>);
+                                           listDOM.push(<Item id={entry[0]}
+                                                              onRemove={this._removeItem}
+                                                              value={entry[1].value}/>);
 
                                        }
                                        for (let entry of this.state.editList) {
 
-                                           editListDOM.push(<ItemEditor id={entry[0]} value={entry[1].value}/>);
+                                           editListDOM.push(<ItemEditor
+                                               onRemove={this._removeItemEditor}
+                                               id={entry[0]}
+                                               value={entry[1].value}/>);
 
                                        }
 
                                        return ( <ul>
-                                           <button onClick={this.add} className="btn btn-default">
+                                           <button onClick={this._add} className="btn btn-default">
                                                Add
                                            </button>
-                                           {/*<Item vaule="Name:  ">My name is yangzy</Item>
-                                            <Item vaule="Age: ">I am 18</Item>
-                                            <Item vaule="Sex: ">girl</Item>
-                                            <ItemEditor value='yangzy'/>*/}
                                            {listDOM}
                                            {editListDOM}
                                        </ul>);
@@ -70,15 +87,34 @@ const Item = React.createClass({
 
 const ItemEditor = React.createClass({
 
+                                         getInitialState(){
+                                             return {
+                                                 value: '',
+
+                                             }
+                                         },
+
+                                         remove(){
+                                             this.props.onRemove(this.props.id);
+                                         },
+                                         _changeHandle(event){
+                                             this.setState({
+                                                               value: event.target.value,
+                                                           });
+                                             /*this.state.value = event.target.value;
+                                              this.forceUpdate();*/
+                                         },
                                          render(){
                                              return (
                                                  <li className="list-group-item">
                                                      <input className="item-edit"
-                                                            defaultValue={this.props.value}/>
+                                                            onChange={this._changeHandle}
+                                                            value={this.state.value}/>
                                                      <a href="#"
                                                         className="glyphicon glyphicon-share"></a>
                                                      <a href="#"
-                                                        className="glyphicon glyphicon-remove"></a>
+                                                        className="glyphicon glyphicon-remove"
+                                                        onClick={this.remove}></a>
                                                  </li>);
                                          }
                                      });
