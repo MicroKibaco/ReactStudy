5eaef741d8da43061d0dae0ef4136d0fa49d23e2
MicroKibaco <yangzy3@asiainfo.com>
2017-05-30 16:09:57 +0800

    Form attribute

--- Chapter04/js/form.js
+++ Chapter04/js/form.js
@@ -5,36 +5,65 @@ const Form = React.createClass({
 
                                    getInitialState(){
                                        return {
-                                           inputValue: 'TinyBin'
+                                           inputValue: 'TinyBin',
+                                           inputValue2: '',
+                                           checkboxValue: false,
+                                           radioValue: false,
                                        }
                                    },
+
                                    changeHandle(event){
-                                     /*  this.state.inputValue = event.target.value;
-                                       this.forceUpdate();*/
+                                       /*  this.state.inputValue = event.target.value;
+                                        this.forceUpdate();*/
                                        this.setState({
 
-                                                         inputValue:event.target.value
+                                                         inputValue: event.target.value
+                                                     });
+                                   },
+
+                                   changeHandle2(event){
+
+                                       this.state.inputValue2 = event.target.value
+
+                                   },
+                                   changeCheckboxHandle(){
+                                       this.setState({
+
+                                                         checkboxValue: !this.state.checkboxValue
+                                                     });
+                                   },
+                                   changeRadioHandle(){
+                                       this.setState({
+
+                                                         radioValue: !this.state.radioValue
                                                      });
                                    },
                                    render(){
                                        return <form >
                                            <input type="text" value={this.state.inputValue}
                                                   onChange={this.changeHandle}/>
-                                           <input type="checkbox"/>
-                                           <input type="radio"/>
-                                           <select>
-                                               <option value="">A</option>
-                                               <option value="">B</option>
-                                               <option value="">C</option>
+                                           <input type="text" defaultValue='yangzy'
+                                                  onChange={this.changeHandle2}/>
+                                           <input type="checkbox" checked={this.state.checkboxValue}
+                                                  onChange={this.changeCheckboxHandle}/>
+                                           <input type="checkbox" defaultChecked/>
+                                           <input type="radio" checked={this.state.radioValue}
+                                                  onChange={this.changeRadioHandle}/>
+                                           <select value="B">
+                                               <option value="A">A</option>
+                                               <option value="B">B</option>
+                                               <option value="C">C</option>
                                            </select>
-                                           <select multiple>
-                                               <option value="">A</option>
-                                               <option value="">B</option>
-                                               <option value="">C</option>
+                                           <select multiple value={['B','C']}>
+                                               <option value="A">A</option>
+                                               <option value="B">B</option>
+                                               <option value="C">C</option>
                                            </select>
-                                           <textaera></textaera>
+                                           <textarea value="569015640"> </textarea>
                                        </form>
                                    }
                                });
 
-ReactDOM.render(<Form/>, document.body);
+setTimeout(function () {
+    ReactDOM.render(<Form/>, document.body);
+}, 1000);
